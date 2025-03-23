110ab7e6aa0accb4cde372c439a7782dbcb540cf
Tom <tomo.sablica@gmail.com>
2025-03-23 12:22:58 +0100

    script is creating a json file with output data

--- webScrap.py
+++ webScrap.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+import re
 
 brochures = []
 
@@ -14,5 +17,49 @@ for div in allCatalogues:
     titleTag = div.find("strong")
     title = titleTag.text.strip() if titleTag else "Unknown"
 
+    img_tag = div.find("img")
+    if img_tag:
+        thumbnail = img_tag.get("src") or img_tag.get("data-src", "")
+    else:
+        thumbnail = ""
+
     logoTag = div.find("img", class_="lazyloadLogo")
     shopName = logoTag["alt"].replace("Logo ", "") if logoTag else "Unknown"
+
+    dateTexts = div.find_all("small")
+
+    validFrom, validTo = "", ""
+
+    if dateTexts:
+        dateText = dateTexts[0].text.strip()
+
+        if " - " in dateText:
+            dateParts = dateText.split(" - ")
+            if len(dateParts) == 2:
+                validFrom, validTo = dateParts
+
+        elif "von" in dateText:
+            match = re.search(r"(\d{2}\.\d{2}\.\d{4})", dateText)
+            if match:
+                validFrom = match.group(1)
+                validTo = ""
+
+    if validFrom:
+        validFrom = datetime.strptime(validFrom, "%d.%m.%Y").strftime("%Y-%m-%d")
+    if validTo:
+        validTo = datetime.strptime(validTo, "%d.%m.%Y").strftime("%Y-%m-%d")
+
+
+    parsedTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    brochures.append({
+        "title": title,
+        "thumbnail": thumbnail,
+        "shop_name": shopName,
+        "valid_from": validFrom,
+        "valid_to": validTo,
+        "parsed_time": parsedTime
+    })
+
+with open("brochures.json", "w", encoding="utf-8") as f:
+    json.dump(brochures, f, indent=4, ensure_ascii=False)
